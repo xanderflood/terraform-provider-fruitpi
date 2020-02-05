82efef067f21f2583be29a79180962feb4afde7c
Xander Flood <xander.t.flood@gmail.com>
2020-02-04 20:44:13 -0500

    cleanup

--- build_artifacts.sh
+++ build_artifacts.sh
@@ -6,14 +6,14 @@ rm -rf terraform.d/
 
 # build for each OS
 mkdir -p terraform.d/darwin_amd64
-GOOS=darwin go build -mod=vendor -o terraform.d/darwin_amd64/terraform-provider-thetradedesk_${DRONE_TAG}
+GOOS=darwin go build -mod=vendor -o terraform.d/darwin_amd64/terraform-provider-fruitpi_${DRONE_TAG}
 
 mkdir -p terraform.d/linux_amd64
-GOOS=linux go build -mod=vendor -o terraform.d/linux_amd64/terraform-provider-thetradedesk_${DRONE_TAG}
+GOOS=linux go build -mod=vendor -o terraform.d/linux_amd64/terraform-provider-fruitpi_${DRONE_TAG}
 
 # bundle them all up
 rm -rf dist
 mkdir dist/
-tar -zcvf dist/terraform-provider-thetradedesk_${DRONE_TAG}.tar.gz terraform.d/
+tar -zcvf dist/terraform-provider-fruitpi_${DRONE_TAG}.tar.gz terraform.d/
 
-md5sum dist/terraform-provider-thetradedesk_${DRONE_TAG}.tar.gz
+md5sum dist/terraform-provider-fruitpi_${DRONE_TAG}.tar.gz
