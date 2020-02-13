ccc8e89e3192790b4b12828cdc625a11c8d8a912
Xander Flood <xander.t.flood@gmail.com>
2020-02-13 18:55:09 -0500

    refactor build_artifacts

--- build_artifacts.sh
+++ build_artifacts.sh
@@ -4,12 +4,16 @@ set -ste
 
 rm -rf terraform.d/
 
+export CGO_ENABLED=0
+export GOARCH=amd64
+export GOFLAGS="-mod=vendor -a -installsuffix cgo"
+
 # build for each OS
 mkdir -p terraform.d/plugins/darwin_amd64
-CGO_ENABLED=0 GOARCH=amd64 GOOS=darwin go build -mod=vendor -a -installsuffix cgo -o terraform.d/plugins/darwin_amd64/terraform-provider-fruitpi_${DRONE_TAG}
+GOOS=darwin go build -o terraform.d/plugins/darwin_amd64/terraform-provider-fruitpi_${DRONE_TAG}
 
 mkdir -p terraform.d/plugins/linux_amd64
-CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -mod=vendor -a -installsuffix cgo -o terraform.d/plugins/linux_amd64/terraform-provider-fruitpi_${DRONE_TAG}
+GOOS=linux go build -o terraform.d/plugins/darwin_amd64/terraform-provider-fruitpi_${DRONE_TAG}
 
 # bundle them all up
 rm -rf dist
